0b9faec185ccd7be81c96ebd077be5e2a981f90e
John Coonrod <johncoonrod@penguin>
2020-11-11 08:35:53 -0500

    deploy problems

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-use Thpglobal;
+use \Thpglobal;
 require 'vendor/autoload.php';
+
+session_start();
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL & ~E_NOTICE);
+
+
 $page=new Page;
 $page->start("Hello World");
 $page->end();
